06dd6bd6a401c02010401ed345e8e96a62208668
nguyenvanhuy656688 <xpham9668@gmail.com>
2022-09-22 21:16:54 +0700

    bai_tap 22/09/2022

---
+++ modul2/src/ss13_search_algorithm/exercise/StringMax.java
@@ -0,0 +1,31 @@
+package ss13_search_algorithm.exercise;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class StringMax {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LinkedList<Character> list = new LinkedList<>();
+        LinkedList<Character> max = new LinkedList<>();
+
+        System.out.println("nhập chuỗi đê bạn êiii");
+        String string = scanner.nextLine();
+
+        for (int i = 0; i < string.length(); i++) {
+            if (list.size() > 1 && string.charAt(i) <= list.getLast() && list.contains(string.charAt(i))) {
+                list.clear();
+            }
+            list.add(string.charAt(i));
+
+            if (list.size() > max.size()) {
+                max.clear();
+                max.addAll(list);
+            }
+        }
+        for (Character chars : max) {
+            System.out.print(chars);
+        }
+        System.out.println();
+    }
+}
