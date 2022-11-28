ae587fa3300430cbbfa7c5aa39e87f8ccabe67b3
nguyenvanhuy656688 <xpham9668@gmail.com>
2022-11-28 11:07:55 +0700

    first commit

--- modul3/case_study_jsp/src/main/java/repository/impl_customer/ICustomerTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package repository.impl_customer;
-
-import model.CustomerType;
-
-import java.util.List;
-
-public interface ICustomerTypeRepository {
-     List<CustomerType> selectAll();
-}
--- modul3/case_study_jsp/src/main/java/service/impl_customer_service/ICustomerTypeService.java
+++
@@ -1,9 +0,0 @@
-package service.impl_customer_service;
-
-import model.CustomerType;
-
-import java.util.List;
-
-public interface ICustomerTypeService  {
-     List<CustomerType> selectAll();
-}
